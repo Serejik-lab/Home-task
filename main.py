5104cd580b982720f267f82fa87cd6df736508c2
Serejik-lab <vasilevsergej134@gmail.com>
2024-11-08 18:05:12 +0300

    "variable"

---
+++ main.py
@@ -0,0 +1,5 @@
+home_tasks = (int("12"))
+hours_spent = (float('1.5'))
+course_name = '"Pyton"'
+time_for_one_task = (hours_spent / home_tasks)
+print( "Название курса:", course_name, ",всего задач:", home_tasks, ",затрачено часов:", hours_spent, ",среднее время выполнения:", time_for_one_task)
