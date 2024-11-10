c091baec647a534b0602cd0e8103410c659eafbb
Sergei <vasilevsergej134@gmail.com>
2024-11-10 11:46:32 +0300

    10/11

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите текст: ')
+print('Количество символов в введёном тексте: ', len(my_string)) # C помощью команды len узнаем длину строки в символах
+print(my_string .upper()) #В верхнем регистре
+print(my_string .lower()) #В нижнем регистре
+print(my_string .replace(' ', '')) #Без пробелов
+print(my_string [0])
+print(my_string [-1])
+
